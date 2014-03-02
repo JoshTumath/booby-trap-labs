ad70c2a9133bab957fd9832b5d2bfebdbcbfae95
Josh Tumath <josh@joshtumath.me.uk>
2014-03-02 00:42:25 +0000

    Created basic code structure and main menu

--- js/game.js
+++ js/game.js
@@ -5,22 +5,22 @@
 (function() {
 "use strict";
 
-
 /**
  * A class managing the creation and running of the game. When instanciated, it
  * creates all the elements required for the game to work.
- * @param {HTMLElement} element A placeholder element where the game will be
- *                              created within
+ * @param {HTMLElement} element a placeholder element that will contain the
+ *                              elements for the game
  */
-function Game(element) {
-  this._gameElement = element;
-  this._gameElement.innerHTML =
+function GameManager(element) {
+  // Set up the elements used by the game
+  this._gameManagerElement = element;
+  this._gameManagerElement.innerHTML =
           '<div id="game-menu" hidden>' +
-          '  <h1>Untitled game</h1>' +
+          '  <h1>Booby Trap Tester</h1>' +
 
           '  <ul>' +
           '    <li>' +
-          '      <button type="button">Play</button>' +
+          '      <button type="button">Start</button>' +
           '    </li>' +
           '    <li>' +
           '      <button type="button">Options</button>' +
@@ -33,7 +33,7 @@ function Game(element) {
           
           '<canvas id="game-canvas" width="640" height="480" hidden></canvas>' +
 
-          '<div id="game-options" class="game-dialog" hidden>' +
+          '<div id="game-options" hidden>' +
           '  <h2>Options</h2>' +
           '  <label>' +
           '    <input type="checkbox">' +
@@ -41,41 +41,119 @@ function Game(element) {
           '  </label>' +
           '</div>' +
 
-          '<div id="game-help" class="game-dialog" hidden>' +
+          '<div id="game-help" hidden>' +
           '  <h2>Help</h2>' +
           '  <p>Foo</p>' +
           '</div>';
   
-  this._gameMenuElement = document.getElementById("game-menu");
-  this._gameOptionsElement = document.getElementById("game-options");
-  this._gameHelpElement = document.getElementById("game-help");
-  this._gameCanvasElement = document.getElementById("game-canvas");
+  // Instantiate the objects that deal with each element
+  this._game = new Game(document.getElementById("game-canvas"));
+  this._menu = new Menu(document.getElementById("game-menu"));
+  this._options = new Dialog(document.getElementById("game-options"));
+  this._help = new Dialog(document.getElementById("game-help"));
+  
+  // Start the system
+  this._menu.initMenuEvents(this._game, this._options, this._help);
+  this._menu.show();
 }
 
-Game.prototype.showMenu = function() {
-  // TODO: Destroy current game
-  this._gameMenuElement.hidden = false;
+
+/**
+ * A class dealing with the main menu of the game. It includes buttons for
+ * starting the game, viewing the options and viewing the help information.
+ * @param {HTMLElement} element a placeholder element that will contain the
+ *                              main menu of the game
+ */
+function Menu(element) {
+  this._menuElement = element;
+}
+
+Menu.prototype = {
+  show: function () {
+    this._menuElement.hidden = false;
+  },
+  
+  hide: function () {
+    this._menuElement.hidden = true;
+  },
+  
+  initMenuEvents: function (game, options, help) {
+    var self = this;
+    var buttons = this._menuElement.getElementsByTagName("button");
+    
+    // Game
+    buttons[0].addEventListener("click", function () {
+      self.hide();
+      game.start();
+    }, false);
+    
+    // Options
+    buttons[1].addEventListener("click", function () {
+      options.open();
+    }, false);
+    
+    // Help
+    buttons[2].addEventListener("click", function () {
+      help.open();
+    }, false);
+  }
 };
 
-Game.prototype.showHelpDialog = function() {
-  this._gameHelpElement.hidden = false;
+
+/**
+ * A class that creates a simple dialog box that appears above what is currently
+ * being displayed.
+ * @param {HTMLElement} element an element containing the contents of the dialog
+ *                              box
+ */
+function Dialog(element) {
+  this._windowElement = element;
+  
+  this._windowElement.className = "game-dialog";
+  this._windowElement.appendChild(this._createCloseButton());
+}
+
+Dialog.prototype = {
+  open: function () {
+    this._windowElement.hidden = false;
+  },
+  
+  close: function () {
+    this._windowElement.hidden = true;
+  },
+  
+  _createCloseButton: function () {
+    var self = this;
+    
+    var image = document.createElement("img");
+    image.src = "images/close.png";
+    image.alt = "Close dialog";
+    
+    var button = document.createElement("button");
+    button.className = "game-dialog-close";
+    button.appendChild(image);
+    
+    button.addEventListener("click", function () {
+      self.close();
+    }, false);
+    
+    return button;
+  }
 };
 
 
-function Grid() {
-  this._tiles; // TODO
+function Game() {
+  // TODO
 }
 
+Game.prototype = {
+  start: function () {
+    // TODO
+  }
+};
 
-function Tile() {
-  this._image; // TODO
-}
 
+// BEGIN SCRIPT
+new GameManager(document.getElementById("game"));
 
-function main() {
-  var game = new Game(document.getElementById("game"));
-  game.showMenu();
-}
-
-main();
 })();
